ded50468b18de42c7e2a4813165bc2a5ea777f40
skylinec <xmattholloway@gmail.com>
2023-05-10 15:26:54 +0100

    remove .

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Matthew Holloway';
-export const SITE_DESCRIPTION = 'I am a student currently studying at Goldsmiths with a keen interest in technology, computer science, music and the intersection between these disciplines.';
+export const SITE_DESCRIPTION = 'I am a student currently studying at Goldsmiths with a keen interest in technology, computer science, music and the intersection between these disciplines';
